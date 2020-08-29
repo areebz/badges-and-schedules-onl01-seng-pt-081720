612e0c3a0aae7dbd85a0afe1dc389611180799b3
Areeba Zahir <areebazahir3@gmail.com>
2020-08-29 00:47:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,9 +4,8 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-   #create new array to put name inside
    array = []
-   #loop through each name provided and push the message to new array with the name
+
    attendees.each { |name| array << badge_maker(name) }
    array #return the new array
 end
