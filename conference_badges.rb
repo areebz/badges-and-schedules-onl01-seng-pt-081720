650fa91a14a897e28bee771fd0f5fdb73087b3e6
Areeba Zahir <areebazahir3@gmail.com>
2020-08-29 00:48:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,13 @@ def batch_badge_creator(attendees)
    attendees.each { |name| array << badge_maker(name) }
    array
 end
+
+def assign_rooms(attendees)
+   array = []
+   #each_with_index: keep track of the index number of the current iteration
+   attendees.each_with_index do |name, index|
+      array << "Hello, #{name}! You'll be assigned to room #{index+1}!" 
+   end
+   array
+end
+
